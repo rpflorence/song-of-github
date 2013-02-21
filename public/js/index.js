3d7129d3cefba3a51e14ac5a7567039d7ca34965
Ryan Florence <rpflorence@gmail.com>
2013-02-20 23:43:23 -0700

    a little music theory for real music

--- public/js/index.js
+++ public/js/index.js
@@ -22,21 +22,7 @@ var loader = new widgets.Loader();
 
     for(i; i < calendarData.length; i++){
       contrib = calendarData[i][1];
-
-      if(contrib > 0){
-        if(contrib < 5){
-          contrib = 1;
-        }else if(contrib < 10){
-          contrib = 2;
-        }else if(contrib < 15){
-          contrib = 3;
-        }else {
-          contrib = 4;
-        }
-      }
-
       column.push(contrib);
-
       if(i > 0 && ((i+1) % 7 === 0)){
         weeks.push(column);
         column = [];
@@ -70,33 +56,80 @@ var loader = new widgets.Loader();
     }
   }
 
+  var n = 0, delay;
+
   function loadSong(weeks){
     MIDI.loadPlugin({
       instruments: [ 'acoustic_grand_piano' ],
       callback: function() {
-        var n = 0,
-            m = 0,
-            delay, note, velocity;
-
         loader.stop();
         MIDI.programChange(0, 0);
         MIDI.programChange(1, 118);
 
         for(n; n < weeks.length; n++){
-          delay = n / 4; // play one chord every quarter second
-          note = MIDI.pianoKeyOffset + (4 + (12 * 3)) - 1; // the MIDI note
-          velocity = 47; // how hard the note hits
-
-          for(m = 0; m < weeks[n].length; m++){
-            if(weeks[n][m] > 0){
-              MIDI.noteOn(0, note + (weeks[n][m] * 4), velocity, delay + (m / 16));
-            }
-
-            (function(n, m){ window.setTimeout(function(){ updateTD(n,m) }, ((delay + (m / 16)) * 1000)) }(n, m));
-          }
+          delay = n;
+          playWeek(weeks[n], n);
         }
       }
     });
+  }
+
+  var chords = {
+    I:   [48, 52, 55, 60, 64, 67, 72],
+    ii:  [50, 53, 57, 62, 65, 69, 74],
+    iii: [52, 55, 59, 64, 67, 71, 76],
+    IV:  [41, 45, 48, 53, 57, 60, 65],
+    V:   [43, 47, 50, 55, 59, 62, 67],
+    vi:  [45, 48, 52, 57, 60, 64, 69],
+    vii: [47, 50, 53, 59, 62, 65, 71]
+  };
+
+  var chordMap = ['I', 'ii', 'iii', 'IV', 'vi', 'vii'];
+
+  function playWeek(week, n) {
+    var note = 60;
+    var sum = week.reduce(function(t, n) { return t + n; }, 0);
+    var chord = getChord();
+    var arpeggio = week[0] > 0;
+    var noteDelay;
+
+    for(var m = 0; m < week.length; m++){
+      if(week[m] > 0){
+        MIDI.noteOn(0, getNote(), getVelocity(), getDelay());
+        if (m > 5) {
+          MIDI.noteOn(0, getNote(), getVelocity(), getDelay() * 0.5);
+        }
+      }
+
+      (function(n, m){
+        window.setTimeout(function(){
+          updateTD(n,m)
+        }, noteDelay * 1000)
+      }(n, m));
+    }
+
+    function getChord() {
+      var l = chordMap.length;
+      return chords[chordMap[(sum ^ l) % (l - 1)]];
+    }
+
+    function getNote() {
+      var note = chord[m];
+      return (sum % 14 == 0) && (m % 3 == 0) ? note + 1 : note;
+    }
+
+    function getVelocity() {
+      return 20 + (m * 4);
+    }
+
+    function getDelay() {
+      if (arpeggio) {
+        noteDelay = delay + (m / chordMap.length - 1);
+      } else {
+       noteDelay = delay;
+      }
+      return noteDelay;
+    }
   }
 
   $(function(){
